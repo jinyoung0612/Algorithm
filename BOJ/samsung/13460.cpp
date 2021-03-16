e88df649ba740381593ec4c2e5d8e992babeb97d
Choi Jinyoung <bmdj123@gmail.com>
2021-03-16 17:16:51 +0900

    update 13460.cpp

--- BOJ/samsung/13460.cpp
+++ BOJ/samsung/13460.cpp
@@ -1,44 +1,106 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 #define MAX 11
+
 int N,M;
 char map[MAX][MAX];
 int dx[4]={1,-1,0,0};
 int dy[4]={0,0,1,-1};
-bool visited[MAX][MAX]={0,};
-int answer=0;
-bool success=0;
+bool visited[MAX][MAX][MAX][MAX]={0,};
 
-void DFS(int rx, int ry, int bx, int by, int count){
-    if(count==10) return;
-    if(success) return;
+struct balls {
+    int cnt;
+    int rx;
+    int ry;
+    int bx;
+    int by;
+};
 
-    if(map[x][y]=='O'){
-        if(count>10) answer=-1;
-        answer=count;
+void BFS(int rx, int ry, int bx, int by){
+    bool flag=false;
+    queue<balls> q;
+    balls b={0,rx,ry,bx,by};
+    q.push(b);
+    visited[rx][ry][bx][by]=1;
+
+    while(!q.empty()){
+    
+       balls cur=q.front();
+       q.pop();
+        
+        if(cur.cnt>10){
+           cout<<-1;
+           return;
+       }
+
+       if(map[cur.rx][cur.ry]=='O'){
+           flag=true;
+           cout<<cur.cnt;
+           return;
+       }
+       for(int i=0; i<4; i++){
+           int nrx=cur.rx;
+           int nry=cur.ry;
+           int nbx=cur.bx;
+           int nby=cur.by;
+
+           while(1){
+               nrx+=dx[i];
+               nry+=dy[i];
+               if(map[nrx][nry]=='#'){
+                   nrx-=dx[i];
+                   nry-=dy[i];
+                   break;
+               }
+               else if(map[nrx][nry]=='O'){
+                   break;
+               }
+           }
+
+            while(1){
+                nbx+=dx[i];
+                nby+=dy[i];
+               if(map[nbx][nby]=='#'){
+                   nbx-=dx[i];
+                   nby-=dy[i];
+                   break;
+               }
+               else if(map[nbx][nby]=='O'){
+                   break;
+               }
+           }
+
+           if(map[nbx][nby]=='O') continue;
+           
+
+           if(nrx==nbx && nry==nby){
+               switch(i){
+                   case 0: cur.rx<cur.bx ? nrx-- : nbx--; break;
+                   case 1: cur.rx<cur.bx ? nbx++ : nrx++; break;
+                   case 2: cur.ry<cur.by ? nry-- : nby--; break;
+                   case 3: cur.ry<cur.by ? nby++ : nry++; break;
+               }
+
+           }
+
+           if(!visited[nrx][nry][nbx][nby]){
+               balls temp={cur.cnt+1,nrx,nry,nbx,nby};
+               q.push(temp);
+               visited[nrx][nry][nbx][nby]=1;
+           } 
+       }
+    }
+    if(!flag){
+        cout<<-1;
         return;
     }
-
-    
-    for(int i=0; i<4; i++){
-        int nrx=rx+dx[i];
-        int nry=ry+dy[i];
-        int nbx=bx+dx[i];
-        int nby=by+dy[i];
-
-        if(0<=next_x&&next_x<N&0<=next_y&&next_y<M){
-            if(map[next_x][next_y]=='.'&&!visited[next_x][next_y]){
-                visited[next_x][next_y]=1;
-                DFS(next_x,next_y,count+1);
-                
-            }
-        }
-    }
-
+   
 }
+
 int main(void){
    
     cin>>N>>M;
@@ -46,23 +108,26 @@ int main(void){
     for(int i=0; i<N; i++){
         cin>>map[i];
     }
-
+      
     int rx,ry,bx,by;
-    
+
     for(int i=0; i<N; i++){
-        for(int j=0; i<M; j++){
+        for(int j=0; j<M; j++){
             if(map[i][j]=='R'){
-                rx=i,ry=j;
+                rx=i;
+                ry=j;
                 map[i][j]='.';
             }
-            else if(map[i][j]='B'){
-                bx=i, by=j;
+            else if(map[i][j]=='B'){
+                bx=i;
+                by=j;
                 map[i][j]='.';
             }
         }
     }
-    DFS(rx,ry,bx,by,0);
-    cout<<answer;
+
+    BFS(rx,ry,bx,by);
 
     return 0;
 }
+
