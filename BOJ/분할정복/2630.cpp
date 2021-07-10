64e0fbb9972460dc542676d9a214886a8d38bcae
jinyoung0612 <bmdj123@gmail.com>
2021-07-10 23:49:35 +0900

    add

---
+++ BOJ/분할정복/2630.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define MAX 10000001
+
+int main(void){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+ 
+    return 0;
+}
